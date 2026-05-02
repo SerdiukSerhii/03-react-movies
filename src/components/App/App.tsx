6f62b8146fc865e88e8838989135b64dad6ab97b
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-05-03 01:06:38 +0300

    add file form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,15 @@
-// import css from './App.module.css';
-// import axios from 'axios';
+// // import css from './App.module.css';
+// import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  console.log('Hello');
+// function App() {
+//   console.log('Hello');
 
-  return <div>Hello</div>;
-}
+//   return (
+//     <div>
+//       Hello
+//       <SearchBar onSubmit={handleSearch} />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
